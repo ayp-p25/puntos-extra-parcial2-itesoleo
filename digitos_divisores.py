68a3e775290ba9ed5fab7209a8bda3695e962fa2
itesoleo <w12senifo@gmail.com>
2025-03-28 13:42:20 -0600

    Update digitos_divisores.py

--- digitos_divisores.py
+++ digitos_divisores.py
@@ -1,1 +1,7 @@
-# Codifica aquí tu programa
+def contar_divisores(numero):
+    numero_str = str(numero)
+    contador = 0
+    for digito in numero_str:
+        if digito != '0' and numero % int(digito) == 0:
+            contador += 1
+    return contador
